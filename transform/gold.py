9f91201b3400f6866c7d9e4c8e3e972208b27a9c
Ana Ferreira <ani.fb95@gmail.com>
2026-07-27 21:15:34 -0300

    fix: corrige duplicado en merge y renombra columnas de variacion en gold

--- transform/gold.py
+++ transform/gold.py
@@ -39,20 +39,18 @@ def crear_gold():
         (df["venta"] - df["compra"])
         /
         df["venta"]
-        * 100
     ).round(2)
 
 
-    # Orden necesario para cálculo histórico
+    # Orden necesario para calculo historico
 
     df = df.sort_values(
-        ["tipo_dolar", "fecha"]
+        ["tipo_dolar", "fecha", "hora"]
     )
 
+    # LAG venta dia anterior
 
-    # LAG venta día anterior
-
-    df["venta_dia_anterior"] = (
+    df["venta_anterior"] = (
         df
         .groupby("tipo_dolar")["venta"]
         .shift(1)
@@ -62,7 +60,7 @@ def crear_gold():
     df["variacion_absoluta"] = (
         df["venta"]
         -
-        df["venta_dia_anterior"]
+        df["venta_anterior"]
     ).round(2)
 
 
@@ -70,22 +68,94 @@ def crear_gold():
         (
             df["venta"]
             -
-            df["venta_dia_anterior"]
+            df["venta_anterior"]
         )
         /
-        df["venta_dia_anterior"]
-        * 100
+        df["venta_anterior"]
+    ).round(2)
+
+    
+    df["compra_anterior"] = (
+        df
+        .groupby("tipo_dolar")["compra"]
+        .shift(1)
+    )
+
+    df["variacion_absoluta_compra"] = (
+        df["compra"] 
+        -
+        df["compra_anterior"]
+    ).round(2)
+
+    df["variacion_porcentual_compra"] = (
+        (
+            df["compra"]
+            -
+            df["compra_anterior"]
+        )
+        /
+        df["compra_anterior"]
     ).round(2)
 
 
-    # Fecha para análisis visual
+    # Precio promedio entre compra y venta
+
+    df["precio_promedio"] = (
+        (
+            df["compra"] +
+            df["venta"]
+        ) / 2
+    ).round(2)
+
+
+    # Tendencia del precio
+
+    df["tendencia"] = "Sin cambios"
+
+    df.loc[
+        df["variacion_absoluta"] > 0,
+        "tendencia"
+    ] = "Sube"
+
+    df.loc[
+        df["variacion_absoluta"] < 0,
+        "tendencia"
+    ] = "Baja"
+
+    # Calcular la brecha con el oficial (solo para los tipos de dolar que no son oficial)
+    oficial = (
+        df[df["tipo_dolar"] == "oficial"]
+        .sort_values("hora")
+        .groupby("fecha")
+        .tail(1)
+        [["fecha", "venta"]]
+        .rename(columns={"venta": "venta_oficial"})
+    )
+
+    df = df.merge(
+        oficial,
+        on="fecha",
+        how="left"
+    )
+
+   # Calcular la brecha con el oficial
+    df["brecha_oficial"] = (
+        (
+            df["venta"] - df["venta_oficial"]
+        )
+        /
+        df["venta_oficial"]
+    ).round(2)
+
+    df.drop(columns=["venta_oficial"], inplace=True)
+
+    # Fecha para analisis visual
 
     df["fecha_formateada"] = (
         pd.to_datetime(df["fecha"])
         .dt
         .strftime("%d-%m-%Y")
     )
-
 
     # 3. Guardar Gold en BigQuery
 
